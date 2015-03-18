a7a7b95e8b05a3229ef99a89972c1c9d4745808a
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-03-17 22:06:36 -0400

    Initial Commit
    First commit for angular Wait and Eat project through learn angular
    course

---
+++ app/js/controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  
+  .controller('LandingPageController', [function() {
+    
+  }])
+  .controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
