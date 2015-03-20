c40a71ac4428910cb8240b95b1355d1c13a92d73
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-03-19 20:14:57 -0400

    Adding controller logic for waitlist page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,11 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function() {
 
   }])
-  .controller('WaitlistController', [function() {
-    // body...
+  .controller('WaitlistController', ['$scope', function($scope) {
+    $scope.parties = [];
+    $scope.party = {name: '', phone: '', size: ''};
+    $scope.saveParty = function() {
+      $scope.parties.push($scope.party);
+      $scope.party = {name: '', phone: '', size: ''};
+    };
   }]);
