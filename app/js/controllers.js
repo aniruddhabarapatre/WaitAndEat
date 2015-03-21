077d159e5f0b9249a5fe317f9139ccdbf544ec07
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-03-20 23:32:23 -0400

    Coding for sms messages

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('myApp.controllers', [])
   }])
   .controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
     // create firebase data
-    var partiesRef = new Firebase('https://waitandeat-angular.firebaseio.com/');
+    var partiesRef = new Firebase('https://waitandeat-angular.firebaseio.com/parties');
     // new object that represents data
     $scope.parties = $firebase(partiesRef);
     $scope.newParty = {name: '', phone: '', size: ''};
@@ -17,5 +17,12 @@ angular.module('myApp.controllers', [])
       // adding data to firebase
       $scope.parties.$add($scope.newParty);
       $scope.newParty = {name: '', phone: '', size: ''};
+    };
+
+    // Send text message to party
+    $scope.sendSMS = function(phoneNumber) {
+      var textMessageRef = new Firebase('https://waitandeat-angular.firebaseio.com/textMessages');
+      var textMessages = $firebase(textMessageRef);
+      textMessages.$add({phoneNumber: phoneNumber});
     };
   }]);
