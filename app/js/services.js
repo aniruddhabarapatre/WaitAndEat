4ba67daa49fb84712ea046a17e817ba5b3ed2432
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-03-28 21:05:37 -0400

    Creating dataService to reduce repititive firebase code

--- app/js/services.js
+++ app/js/services.js
@@ -7,11 +7,14 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
   .value('FIREBASE_URL', 'https://waitandeat-angular.firebaseio.com/')
-  .factory('partyService', function($firebase, FIREBASE_URL) {
-    // create firebase data
-    var partiesRef = new Firebase(FIREBASE_URL + 'parties');
-    // new object that represents data
-    var parties = $firebase(partiesRef);
+  .factory('dataService', function($firebase, FIREBASE_URL) {
+    var dataRef = new Firebase(FIREBASE_URL);
+    var fireData = $firebase(dataRef);
+    return fireData;
+  })
+  .factory('partyService', function(dataService) {
+    // using child to get parties in firebase
+    var parties = dataService.$child('parties');
     var partyServiceObject = {
       parties: parties,
       saveParty: function(party) {
@@ -21,9 +24,8 @@ angular.module('myApp.services', [])
 
     return partyServiceObject;
   })
-  .factory('textMessageService', function($firebase, FIREBASE_URL, partyService) {
-    var textMessageRef = new Firebase(FIREBASE_URL + 'textMessages');
-    var textMessages = $firebase(textMessageRef);
+  .factory('textMessageService', function(dataService, partyService) {
+    var textMessages = dataService.$child('textMessages');
     var textMessageServiceObject = {
       sendTextMessage: function(party) {
         var newTextMessage = {
